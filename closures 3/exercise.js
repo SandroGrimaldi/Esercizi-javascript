9c812ab4e3e27cca126076429bf2ef273b61a453
Sandro Grimaldi <131904800+SandroGrimaldi@users.noreply.github.com>
2023-05-31 17:19:39 +0200

    Update exercise.js, exercise 8 done

--- closures 3/exercise.js
+++ closures 3/exercise.js
@@ -1,3 +1,13 @@
 function multiplyByTwo(value) {
   let number = 2;
+  function inner(){
+    return value * number;
+  }
+  return inner;
 }
+
+console.log(multiplyByTwo(3)());
+console.log(multiplyByTwo(5)());
+console.log(multiplyByTwo(10)());
+console.log(multiplyByTwo(17)());
+console.log(multiplyByTwo(34)());
