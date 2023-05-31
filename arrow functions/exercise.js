4f910c3561c2710bc60aa360a5da7388a8a69d5c
Sandro Grimaldi <131904800+SandroGrimaldi@users.noreply.github.com>
2023-05-31 14:00:19 +0200

    Update exercise.js
    esercizio 3 fatto

--- arrow functions/exercise.js
+++ arrow functions/exercise.js
@@ -1,4 +1,4 @@
-const sum = function (a, b) {
+/* const sum = function (a, b) {
   return a + b;
 }
 
@@ -16,4 +16,16 @@ const divide = function (a, b) {
 
 const log = function (value) {
   console.log(value);
-}
+} */
+
+const sum = (a,b) => a + b;
+
+const subtract = (a,b) => a - b;
+
+const multiply = (a,b) => a * b;
+
+const divide = (a,b) => a / b;
+
+const log = (value) => console.log(value);
+
+log (divide (subtract (multiply (sum (2,4), sum (5,2)), 2), 5))
