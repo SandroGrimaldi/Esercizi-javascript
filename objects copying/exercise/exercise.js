39673c6b77f373aadb9014e392221e82ba5ed9c0
Sandro Grimaldi <131904800+SandroGrimaldi@users.noreply.github.com>
2023-05-31 16:57:46 +0200

    Update exercise.js, exercise 5 done

--- objects copying/exercise/exercise.js
+++ objects copying/exercise/exercise.js
@@ -8,5 +8,11 @@ const person2 = person1;
 
 // Modify the property `firstName` of the `person2` in "Simon".
 
+
+person2.firstName = "Simon";
+
+/* Ho usato questo metodo perchè essendo person1 un oggetto, gli passo un valore per riferimento,
+  cioè posso modificare il valore della key dopo aver "copiato" tutte le keys in person2 */
+
 console.log(person1);
 console.log(person2);
