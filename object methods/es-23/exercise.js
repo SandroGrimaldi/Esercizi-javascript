c95f6a46c494a4751430a249f5ed91ee874b2283
Sandro Grimaldi <131904800+SandroGrimaldi@users.noreply.github.com>
2023-05-31 17:56:06 +0200

    Update exercise.js, exercise 9 done

--- object methods/es-23/exercise.js
+++ object methods/es-23/exercise.js
@@ -5,3 +5,7 @@ const person = {
 }
 
 // Print values of person using Object.keys
+
+for(i=0; i<Object.keys(person).length; i++){
+  console.log (Object.keys(person)[i]+": "+Object.values(person)[i])
+}
