9a014e75e86c1dde855e5a58423f7ada2a2b7481
Sandro Grimaldi <131904800+SandroGrimaldi@users.noreply.github.com>
2023-05-31 18:47:59 +0200

    Update exercise.js, exercise 10 done

--- optional chaining/exercise.js
+++ optional chaining/exercise.js
@@ -3,3 +3,8 @@ const order = {};
 if (order && order.customer && order.customer.address && !order.customer.address.city) {
   console.log('City is required');
 }
+
+else if(!order?.customer?.address?.city) {
+  console.log('City is required')
+}
+/* così verifica tutte le keys */
