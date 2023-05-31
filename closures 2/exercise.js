da501f6e6ba46e48d057826ae4e9e53de82dd424
Sandro Grimaldi <131904800+SandroGrimaldi@users.noreply.github.com>
2023-05-31 17:11:59 +0200

    Update exercise.js, exercise 7 done

---
+++ closures 2/exercise.js
@@ -0,0 +1,13 @@
+function printName(){
+    let helloName= "Hello John";
+    function inner(){
+        setTimeout(()=> {
+            console.log(helloName);
+        },1000);
+
+        return helloName;
+    }
+    return inner;
+}
+
+printName()();
