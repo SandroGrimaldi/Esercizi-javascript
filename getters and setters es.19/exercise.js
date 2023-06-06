56b2c1d382e0416c0885149a0d1183e0455674fa
Sandro Grimaldi <131904800+SandroGrimaldi@users.noreply.github.com>
2023-06-06 18:09:39 +0200

    Update exercise.js, exercise 19 done

--- getters and setters es.19/exercise.js
+++ getters and setters es.19/exercise.js
@@ -1,5 +1,39 @@
 class Person {
-  // ...
+  
+  #firstName= '';
+  #lastName= '';
+  #age= 0; 
+
+constructor(firstName,lastName,age){
+  this.firstName= firstName;
+  this.lastName= lastName;
+  this.age=age;
+}
+
+set firstName(fNVal){
+  this.#firstName=fNVal;
+}
+
+set lastName(lNVal){
+  this.#lastName=lNVal;
+}
+
+set age(ageVal){
+  if(ageVal<=1){
+    this.#age=1;
+  }
+  else if(ageVal>=110){
+    this.#age=110;
+  }
+  else{
+    this.#age=ageVal
+  }
+}
+
+get fullName(){
+  return this.#firstName+" "+this.#lastName+" "+this.#age;
+}
+  
 }
 
 const person = new Person('Mario', 'Rossi', 25);
