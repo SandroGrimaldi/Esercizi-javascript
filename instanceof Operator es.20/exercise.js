b887c4495aacb89589e6837b07abba73c94d1995
Sandro Grimaldi <131904800+SandroGrimaldi@users.noreply.github.com>
2023-06-10 10:53:44 +0200

    Update exercise.js, exercise 20 done

--- instanceof Operator es.20/exercise.js
+++ instanceof Operator es.20/exercise.js
@@ -18,7 +18,26 @@ class Circle {
 }
 
 class AreaCalculator {
-  // ...
+  
+  static calculate(figure){
+    let area = 0;
+    if(figure instanceof Square){
+      area = figure.side * figure.side;
+    }
+    else if (figure instanceof Rectangle){
+      area = figure.width * figure.height;
+    }
+    else if (figure instanceof Circle) {
+      area = Math.round( Math.PI * figure.radius * figure.radius );
+      /* per ottenere un valore intero, altrimenti senza il Math.round che approssima il valore
+        si ottiene 78.53981... */
+    }
+    else{
+      console.log("not a valid geometrical figure")
+    }
+    return area;
+  }
+  
 }
 
 const square = new Square(4);
