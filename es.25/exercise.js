922d7dde220e6c123ab7706c2aab0c913ebe63f1
Sandro Grimaldi <131904800+SandroGrimaldi@users.noreply.github.com>
2023-06-15 19:51:12 +0200

    Add files via upload

---
+++ es.25/exercise.js
@@ -0,0 +1,6 @@
+let numberStore = [0, 1, 2];
+let newNumber = 3;
+/*numberStore.push(newNumber);
+console.log(numberStore);*/
+let newNumberStore =[...numberStore,newNumber];
+console.log(newNumberStore);
