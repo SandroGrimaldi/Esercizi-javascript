54c41ffbd4a11f8ce88b86d00b3536a35d33d54c
Sandro Grimaldi <131904800+SandroGrimaldi@users.noreply.github.com>
2023-06-15 12:09:27 +0200

    Update exercise.js, exercise 18 fixed

--- static method es.18/exercise.js
+++ static method es.18/exercise.js
@@ -3,9 +3,11 @@ class Person {
     this.firstName = firstName;
     this.lastName = lastName;
   }
-  static fromObject(obj={}){
-    return obj
+  
+  static fromObject(obj){
+    return new Person (obj.firstName, obj.lastName)
   }
+  
 }
 
 const obj = {
