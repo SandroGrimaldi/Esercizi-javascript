7b2173690b1b9b384934415440b79c0e91267d0b
Sandro Grimaldi <131904800+SandroGrimaldi@users.noreply.github.com>
2023-06-25 09:49:04 +0200

    Update exercise.js

--- es.34 Callbacks/exercise.js
+++ es.34 Callbacks/exercise.js
@@ -1,8 +1,9 @@
 function printAsyncName(callback,name){
-    let printHello = setInterval(()=> console.log(callback), 1000);
+    let printHello = setInterval(callback, 1000);
     let printName = setInterval(() => console.log(name), 2000);
-    setTimeout(()=> clearInterval(printHello),5000)
-    setTimeout(()=> clearInterval(printName),5000)
+    setTimeout(()=> clearInterval(printHello),5000);
+    setTimeout(()=> clearInterval(printName),5000);
 }
 
-printAsyncName((`Hello`),'Mario')
+
+printAsyncName(() => console.log(`Hello`) ,'Mario')
