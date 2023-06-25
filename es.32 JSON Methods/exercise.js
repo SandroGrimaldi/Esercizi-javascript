2f4df00fce8768ba0dfa62532dfb90db672cd9dc
Sandro Grimaldi <131904800+SandroGrimaldi@users.noreply.github.com>
2023-06-25 09:26:48 +0200

    Update exercise.js, fixato esercizio 32 ed aggiunto metodo alternativo per risoluzione

--- es.32 JSON Methods/exercise.js
+++ es.32 JSON Methods/exercise.js
@@ -5,8 +5,33 @@ const person = {
   age: 25,
 };
 
-/*const json = JSON.stringify(person);*/
+/*const json = JSON.stringify(person);
 
-const json = JSON.stringify({id:person.id, age:person.age})
+const json = JSON.stringify({id:person.id, age:person.age})*/
+
+function replacer(key,value){
+  if(typeof value === "string"){
+    return undefined;
+  }else{
+    return value;
+  }
+}
+
+const json = JSON.stringify(person,replacer);
+
 
 console.log(json); // Should return: { id: 1, age: 25 }
+
+/* 
+  Metodo alternativo
+  function replacer(obj){
+    let objReplacer={
+      id:obj.id,
+      age:obj.age
+    }
+    return objReplacer;
+  }
+
+  const json = JSON.stringify(replacer(person));
+  console.log(json);
+*/
